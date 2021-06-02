96f17528f92e4eacd08933dab09479e7869aac7a
phantomramen <ali.hernandez9two@gmail.com>
2021-06-01 19:54:17 -0600

    6/01 class

---
+++ groupB/forms2/app.js
@@ -0,0 +1,5 @@
+function validateForm() {
+    // Retrieving the values of form elements
+    //the name of the form in html is contactForm
+        const color = document.contactForm.color.value;
+        };
