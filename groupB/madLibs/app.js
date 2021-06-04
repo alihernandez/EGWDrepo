b200b97ec075d0b45b3c9155a3cf24cceb73697f
phantomramen <ali.hernandez9two@gmail.com>
2021-06-03 22:52:22 -0600

    madLibs

--- groupB/madLibs/app.js
+++ groupB/madLibs/app.js
@@ -2,4 +2,10 @@ function libIt() {
     let storyDiv= document.getElementById("story");
     let person= document.getElementById("person").value;
     let adjective= document.getElementById("adjective").value;
+    let noun= document.getElementById("noun").value;
+
+    storyDiv.innerHTML= (`${person} fell off a ${adjective} ${noun} it's funny right?!?`);
 }
+
+const button= document.getElementById("lib-button");
+button.addEventListener('click', libIt);
