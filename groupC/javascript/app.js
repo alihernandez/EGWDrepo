3cd5005eeccfe0cbe08814095c29c14e38210a2d
phantomramen <ali.hernandez9two@gmail.com>
2021-10-26 20:10:46 -0600

    intro to js

--- groupC/javascript/app.js
+++ groupC/javascript/app.js
@@ -1,5 +1,10 @@
 console.log("hello");
 
+//alerts message on the page
 alert("you suck");
+
+//asks yes or no question
 confirm("do you realize you suck?");
+
+//promts user for input
 prompt("why do you suck so bad?");
