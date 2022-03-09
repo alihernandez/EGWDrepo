cf9da0ca287a94905d5f4976568e9ea2d57b092a
phantomramen <ali.hernandez9two@gmail.com>
2022-03-08 17:51:12 -0700

    added server notes

---
+++ groupC/crudApp/server.js
@@ -0,0 +1,41 @@
+//Dependencies//
+
+
+
+//PORTS//
+
+//Create Server//
+
+
+
+// Sets up the Express app to handle data parsing
+
+
+//Set up DB //
+
+
+//ROUTES//
+//Set up a GET request to render our HTML PAGE
+
+
+// Add
+
+
+
+// View
+
+
+
+//Update
+
+// Delete
+
+
+
+
+
+// Closing the database connection.
+
+
+
+//Start your Server
